56ca6cce460a4532454534cbaf7959839080f3e8
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-04 12:49:19 +0900

    first commit

---
+++ docs/welcomes.py
@@ -0,0 +1,23 @@
+# 변수 선언 후 정의 시 고려점(넣는 값이 문자 or 숫자)
+# 문자 출력
+helloworld = "hello, world! myung jun kim." # 문자형 변수 # ""로 표시 해줘야한다.
+print(helloworld)
+
+# 숫자 합산 출력
+numbers = 3 + 5 # 숫자형 변수
+print(numbers)
+
+# 데이터 타입 : 문자형 or 숫자형 등 변수에 대한 정의
+
+
+# 가정문
+if 5 > 2:
+    print("Five is greater than two!")
+
+# 가정문1
+print(numbers)
+
+if 5 > 2: # 묶음 기호인 : 과 tab은 하나에 쌍
+    pass # 디버깅할때 유용하다.
+    print("Five is greater than two!")
+    print("end")

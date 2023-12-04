d7edd62b850f5875998bafded5bbd0cdc13b3970
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-04 18:57:10 +0900

    new_onehandgame

---
+++ README.md
@@ -0,0 +1,7 @@
+### 코딩테스트
+
+|구분|소스|문제설명|출처|
+|--|--|--|--|
+|입출력과 사칙연산|[python](./README.md)|Hello World!를 출력|[백준 2557](https://www.acmicpc.net/problem/2557)|
+
+
--- docs/quests/onehandgame.py
+++ docs/quests/onehandgame.py
@@ -1,27 +1,35 @@
-a1 = "1"
-a2 = " "
-a3 = "2"
-a4 = " "
-a5 = "3"
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
+a1="1"
+a2=""
+a3="2"
+a4=""
+a5="3"
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# 1 x 2 x 3
 
 a1, a2 = a2, a1
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# x 1 2 x 3
 
-a2, a4 = a4, a2
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
-
-a2, a3 = a3, a2
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
+a3, a4 = a4, a3
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# x 1 x 2 3
 
 a3, a5 = a5, a3
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# x 1 3 2 x
+
+a3, a1 = a1, a3
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# 3 1 x 2 x
+
+a4, a3 = a3, a4
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# 3 1 2 x x
+
+a2, a4 = a4, a2
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# 3 x 2 1 x
 
 a4, a5 = a5, a4
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
-
-a1, a3 = a3, a1
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
-
-a2, a3 = a3, a2
-print("{} {} {} {} {}".format(a1, a2, a3, a4, a5))
+print("{} {} {} {} {}" .format(a1, a2, a3, a4, a5))
+# 3 x 2 x 1

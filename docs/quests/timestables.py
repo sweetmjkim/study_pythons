2c14998fe6911969de2bcb03cbc3988ac046338c
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-05 17:52:24 +0900

    timestables

---
+++ docs/quests/timestables.py
@@ -0,0 +1,16 @@
+# 구구단 5단 단계별 표시
+# 예. 5 * 1 = 5
+#     5 * 2 = 10
+#      ....
+#     5 * 9 = 45
+
+# option) 단수 입력 받아 연산
+
+num_times = 0
+while num_timestables < 45 :
+    pass
+    num_timestables = num_times * 5
+    print("{} * {}"= .format(num_times) (num_timestables))
+    pass
+
+print("구구단 5단 완성!")

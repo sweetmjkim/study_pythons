c6811628204a544cbd9fd475fbed4000bd6d1b8b
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-06 11:51:26 +0900

    timestables_fors

--- docs/quests/timestables_fors.py
+++ docs/quests/timestables_fors.py
@@ -5,3 +5,13 @@ for number in num_number :
     pass
     print("{}*{} = {}".format(int(num_timestable), int(number), int(result)))
 print("구구단 5단 완성!")
+
+print("-----------------------------------------------------------------------------")
+
+num_timestable = 5 # 구구단 단수
+num_number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for number in num_number :
+    result = number * num_timestable
+    pass
+    print("{}*{} = {}".format(num_timestable, number, result))
+print("구구단 5단 완성!")

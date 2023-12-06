005d9e20503e186d6e0e515eded5bbe7481816dc
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-06 17:54:11 +0900

    outputs

--- docs/outputs.py
+++ docs/outputs.py
@@ -23,3 +23,10 @@ if 5 > 2: # 묶음 기호인 : 과 tab은 하나에 쌍
     pass # 디버깅할때 유용하다. pass문 : 아무 동작은 하지않지만 나중에 코드 추가 및 구조를 확인 할때 사용하는 빈 문장 
     print("Five is greater than two!")
     print("end")
+
+# 한줄에 출력
+first = "First"
+second = "Second"
+print("first : {}".format(first), end=", 다음 줄 ")
+print("second : {}".format(second))
+print("End progrem!")

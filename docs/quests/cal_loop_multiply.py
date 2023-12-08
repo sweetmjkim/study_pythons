24260e4bbe24bcf6d41e11b3d881ab43ba88b46a
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-08 20:24:55 +0900

    timestables_fors_function

--- docs/quests/cal_loop_multiply.py
+++ docs/quests/cal_loop_multiply.py
@@ -3,14 +3,14 @@
 # 최소 function 1개 사용
 # 실행 이미지 링크 공유(최소 2회 계산)
 
-# def multiply() : 
-#     num_first = int(input("첫번째 입력값 : "))
-#     num_second = int(input("두번째 입력값 : "))
-#     result = num_first * num_second
-#     return result
-# num_multiply = multiply()
-# print("first * second = {}" .format(num_multiply))
-# print("-------------------------------------------------------------------------")
+def multiply() : 
+    num_first = int(input("첫번째 입력값 : "))
+    num_second = int(input("두번째 입력값 : "))
+    result = num_first * num_second
+    return result
+num_multiply = multiply()
+print("first * second = {}" .format(num_multiply))
+print("-------------------------------------------------------------------------")
 
 while True :
         num_multiply = input("종료를 원하면 'q'키 / 계속 진행은 Enter키 누르세요.")
--- docs/quests/timestables_fors_function.py
+++ docs/quests/timestables_fors_function.py
@@ -12,52 +12,12 @@
 #     print("{}*{} = {}".format(num_timestable, number, result))
 # print("구구단 5단 완성!")
 
-미완성
-
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-def num_number() : 
-    num_timestable = int(input())
-    num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    result = num_timestable * num_list
-    return result
-num_number = num_number()
-print("first * second = {}" .format(num_number))
-print("-------------------------------------------------------------------------")
-
-while True :
-        num_multiply = input("종료를 원하면 'q'키 / 계속 진행은 Enter키 누르세요.")
-        if num_multiply == 'q' :
-             print("곱셈용 계산기 종료!")
-             break
-        else : 
-            num_first = int(input("첫번째 입력값 : "))
-            num_second = int(input("두번째 입력값 : "))
-        result = num_first * num_second
-        print("first * second = {}" .format(result))
-        print("-------------------------------------------------------------------------")
-
-
-
-
-
-
-
-
-
-
-    num_number = ()
-    number = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-    result = number * num_timestable
-    pass
-    print("{}*{} = 구구단{}단 완성".format(num_timestable, num_number, result))
-    return result
-num_number = num_number()
-
-
-# def multiply() : 
-#     num_first = int(input("첫번째 입력값 : "))
-#     num_second = int(input("두번째 입력값 : "))
-#     result = num_first * num_second
-#     return result
-# num_multiply = multiply()
-# print("first * second = {}" .format(num_multiply))
+def timestable():
+    while True:
+        num_multiply = input("구구단 숫자(20, 30, 35) 중 입력하거나 종료를 원하면 'q'를 입력하세요.")
+        if num_multiply == 'q':
+            break
+        num_multiply = int(num_multiply)
+        for num_list in range(1, 10):
+            print("{} * {} = {}".format(num_multiply, num_list, num_multiply * num_list))
+timestable()

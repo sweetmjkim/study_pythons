ac60f077eb594bbf535a52ffc6f992c934d0a706
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-13 09:07:41 +0900

    data_types_questions_mix

--- docs/quests/data_types_questions_mix.py
+++ docs/quests/data_types_questions_mix.py
@@ -1,3 +1,4 @@
+# 1단계 진행
 # list_question_mix = []
 # for x in range(1, 4):
 #     list_input = {}
@@ -27,15 +28,34 @@
 #     print("점수 : {}".format(score))
 #     print("----------" * 7)
 
+# 2단계 진행
+# def quest_round() :
+#     dict_question_mix = {}
+#     dict_question_mix["question"]=input("question :")
+#     dict_question_mix["answer"]=input("answer :")
+#     dict_question_mix["correct_index"]=input("correct_index :")
+#     dict_question_mix["score"]=input("score :")
+#     return dict_question_mix
+# list_question_mix=[]
+# for count in [1,2,3] :
+#     list_question_mix.append(quest_round())
+# print(list_question_mix)
 
+# 3단계 진행(최종)
 def quest_round() :
     dict_question_mix = {}
-    dict_question_mix["question"]=input("question :")
-    dict_question_mix["answer"]=input("answer :")
-    dict_question_mix["correct_index"]=input("correct_index :")
-    dict_question_mix["score"]=input("score :")
+    dict_question_mix["문제"] = input("문제 :")
+
+    # answer 딕셔너리를 생성
+    answer_input = input("보기 (예시: 1.보기 ,2.보기,...) : ")
+    answer_dict = dict(item.split(".") for item in answer_input.split(","))
+    dict_question_mix["보기"] = answer_dict
+
+    dict_question_mix["정답"] = input("정답 :")
+    dict_question_mix["점수"] = input("점수 :")
     return dict_question_mix
-list_question_mix=[]
+
+list_question_mix = []
 for count in [1,2,3] :
     list_question_mix.append(quest_round())
 print(list_question_mix)
